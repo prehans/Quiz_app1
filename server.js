165c3fe1396ba761e426140af866591ee16de4f7
prehans <prehansgupta@gmail.com>
2023-10-01 00:02:07 +0530

    updated

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ dotenv.config();
 connectDb();
 
 const app = express();
-app.use(express.static(path.join(__dirname,"./frontend/build")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 app.get("*",function (req,res){
-  res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
 })
 app.use(express.json());
 
